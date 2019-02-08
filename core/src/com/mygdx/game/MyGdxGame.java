2d7065a50a3032aff724c82773b14552c55101ee
Chobaka78 <34846941+Chobaka78@users.noreply.github.com>
2019-02-08 16:29:36 -0500

    Update 9 - added longer player power up

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -16,20 +16,19 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 public class MyGdxGame extends ApplicationAdapter {
 	//hello world
 	SpriteBatch batch;
-	Paddle player;
+	Paddle Vaus;
 	Ball ball;
 	Bricks bricks;
 	Texture texture;
-	public int x = 291;
+	public int x = 291, width;
 	public boolean play = false;
-
 
 	@Override
 	public void create() {
 		texture = new Texture(Gdx.files.internal("Arkanoid1.png"));
 		batch = new SpriteBatch();
-		player = new Paddle(x, 0);
-		ball = new Ball(player.getX() + 27, 10);
+		Vaus = new Paddle();
+		ball = new Ball(Vaus.getX() + 27, 10);
 		bricks = new Bricks();
 
 	}
@@ -41,21 +40,23 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		//keyboard control
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && player.getX() < 582) {
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && Vaus.getX() < 582) {
 			x += 5;
 
 
-		} else if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && player.getX() > 25) {
+		} else if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && Vaus.getX() > 25) {
 			x -= 5;
 		}
-
-		player.setX(x); // set the paddle to centre
+		else if(Gdx.input.isKeyPressed(Input.Keys.K)){
+			width = 600;
+		}
+		Vaus.setX(x); // set the paddle to centre
 		batch.begin();
 
 		batch.draw(texture, 0, 0, 672, 768);
+		Vaus.update(batch,x,Vaus.getY());
 		bricks.update(batch);
 		batch.end();
-		player.render(player.getX(), 0);
 		ball.move(); // this will call the move method in the ball class
 
 	}
--- core/src/com/mygdx/game/Paddle.java
+++ core/src/com/mygdx/game/Paddle.java
@@ -1,34 +1,63 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import sun.java2d.windows.GDIBlitLoops;
+
+import java.util.ArrayList;
 
 
 public class Paddle {
     private static int x, y;
     public static int width, height;
-    private Texture img;
-    static Sprite player;
+    private Texture img, img1;
+    static Sprite player, player1;
     private  SpriteBatch batch;
+    public static int [] VausType = new int[] {0,1};
+    public static ArrayList<Sprite> Vaus = new ArrayList<Sprite>();
+    private static ArrayList<Texture> load = new ArrayList<Texture>();
+    public static int Vaustype;
 
-    public Paddle (int x, int y){
+    public Paddle (){
         Paddle.x = x;
         Paddle.y = y;
         Paddle.width = 500;
         Paddle.height = 12;
-        img = new Texture("player.png");
+        load.add(img = new Texture("player.png"));
+        load.add(img = new Texture("player1.png"));
         player = new Sprite(img);
+        for(int i = 0; i < load.size(); i ++){
+            Vaus.add(new Sprite(load.get(i)));
+        }
+
         batch = new SpriteBatch();
     }
 
-    public void render(int x, int y){
-        batch.begin();
-        player.setPosition(x,y);
-        player.draw(batch);
-        batch.end();
+    public void render(SpriteBatch batch, int type){
+        for( int i = 0; i < Vaus.size(); i ++){
+            if(type == VausType[i]){
+                Vaus.get(type).draw(batch);
+            }
+        }
 
+    }
+    public void update(SpriteBatch batch, int x, int y){
+        if(Gdx.input.isKeyPressed(Input.Keys.K)){
+            Vaustype = 1;
+        }
+        else if(Gdx.input.isKeyPressed(Input.Keys.L)){
+            Vaustype = 0;
+        }
+        for(int i = 0; i < Vaus.size(); i ++){
+            if(Vaustype == VausType[i]){
+                Vaus.get(Vaustype).setPosition(x,y);
+                this.render(batch,Vaustype);
+            }
+        }
     }
 
 
@@ -43,4 +72,5 @@ public class Paddle {
     public void setX(int x){
         Paddle.x = x;
     }
+
 }
